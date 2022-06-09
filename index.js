506f5a2ce1c0a1a64a24b186ecdb6a5a55f50cbc
bran don <106029484+brandonedwinogola@users.noreply.github.com>
2022-06-09 09:06:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-
 // Write your code here
+function mult(num1, num2) {
+    return num1 * num2
+}
+const multNum = mult(31, 2)
+console.log(multNum); // Expected output:62
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    let rand = Math.random() * maxLimit;
+    console.log(rand); // say 99.81321410836433
+
+    rand = Math.floor(rand); // 99 
+
+    return rand;
+
+}
+generate randNumber // 48
+
+function mod(num3 % num4); {
+
+    const % (mod); % (16, 4);
+
+    console.log(mod); // expected output 4
+
+}
+
+
+Math.max([num5, num6, ...getRndInteger.]);
+
+console.log(Math.max(10, 20));
+
+// Expected output 20
